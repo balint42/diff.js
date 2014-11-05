1a5dbcf6217149ab5d0866be4593c68a9ecd6350
Balint Morvai <balint@morvai.de>
2014-11-05 15:51:23 +0100

    Added functions "diff"&"integral";renamed "diff1&2" to "extrema"&"extremaXY"
    "diff" now calculates the differential, "integral" the integral while "extrema" and "extremaXY" look for local minima & maxima.

--- diff.js
+++ diff.js
@@ -1,7 +1,83 @@
-var diff1, diff2;
+var diff, integral, extrema, extremaXY;
 
 (function() {
     /**
+     * Calculate differential of a vector.
+     *
+     * Takes an object or array X with m numbers and returns an array
+     * Y with m-1 numbers that constitutes the differential:
+     * Y = [X(2)-X(1) X(3)-X(2) ... X(m)-X(m-1)]
+     *
+     * If as second parameter a number n is given, the returned array
+     * Y will be the n-th differential, thus above step applied n-times.
+     *
+     * @author Balint Morvai <balint@morvai.de>
+     * @license http://en.wikipedia.org/wiki/MIT_License MIT License
+     * @param values
+     * @param n
+     * @returns Array
+     */
+    diff = function(values, n) {
+        // recursive calls to get n-th differential
+        if(n > 1) {
+            values = diff(values, n-1);
+        }
+        // loop through 1,...,m-1 entries of values to get diff
+        var keys = Object.keys(values);
+        for (var k = 0; k < keys.length-1; k++) {
+            values[k] = values[keys[k+1]]-values[keys[k]];
+        }
+        values.pop(); // last element is old value, delete it
+
+        return values;
+    }
+
+    /**
+     * Calculate one integral of a vector assuming that the variable
+     * constant is zero.
+     *
+     * Takes an object or array Y with m numbers and returns an array
+     * X with m numbers that constitutes the differential:
+     * [ ... -Y(m)-Y(m-1)-Y(m-2)  -Y(m)-Y(m-1)  -Y(m)]
+     *
+     * If as second parameter a number n is given, the returned array
+     * X will be the n-th integral, thus above step applied n-times.
+     *
+     * NOTE: the integral cannot determine the original set of values
+     * before "diff" was applied, thus "integral(diff(values)) != values"
+     * due to the nature of integration and differentiation. But due to
+     * the same nature the following always applies:
+     * "integral(diff(values)) + values(m) = values(1:m-1)"
+     * In other words: to reverse a "diff", apply an integral and add
+     * the last original value "values(m)" to all elements of the resulting
+     * array. Then append the last original value and you have the exact
+     * original "values" array.
+     *
+     * @author Balint Morvai <balint@morvai.de>
+     * @license http://en.wikipedia.org/wiki/MIT_License MIT License
+     * @param values
+     * @param n
+     * @param c
+     * @returns Array
+     */
+    integral = function(values, n) {
+        // recursive calls to get n-th integral
+        if(n > 1) {
+            values = integral(values, n-1);
+        }
+        // loop through m,...,1 entries of values to get integral
+        var keys = Object.keys(values);
+        values[keys.length-1] = -values[keys.length-1];
+        for (var k = keys.length-2; k >= 0; k--) {
+            values[k] = -values[keys[k]]+values[keys[k+1]];
+        }
+
+        return values;
+    }
+
+    /**
+     * Find local maxima and minima in a list of values.
+     *
      * Javascript implementation of:
      * "A Linear-Time Algorithm That Locates Local Extrema
      * of a Function of One Variable From Interval Measurement
@@ -11,12 +87,12 @@ var diff1, diff2;
      * Takes an object or array with numbers and returns an object
      * with two lists of indices: 'minlist' with indices of values
      * that are local minima and 'maxlist' with indices of values that
-     * are local maxima.
+     * are local maxima. Indices can be of any type.
      * Takes numbers as first parameter and an accuracy > 0 (epsilon)
      * as second parameter. The accuracy has to be chosen depending
      * on the fluctuations in the data: smaller values mean greater
-     * reliability in finding extrema but slower computation.
-     * Epsilon defaults to 0.1 if omitted but don't rely on that!
+     * reliability in finding extrema but also greater chance of
+     * confusing noise with a local minimum or maximum.
      *
      * @author Balint Morvai <balint@morvai.de>
      * @license http://en.wikipedia.org/wiki/MIT_License MIT License
@@ -24,7 +100,7 @@ var diff1, diff2;
      * @param eps
      * @returns {minlist: Array, maxlist: Array}
      */
-    diff1 = function(values, eps) {
+    extrema = function(values, eps) {
         // declare local vars
         var x, y;
         // define x & y enumerated arrays
@@ -37,14 +113,16 @@ var diff1, diff2;
             return arr;
         }
         y = enumerate(values);
-        x = Object.keys(values).map(Math.floor);
+        x = Object.keys(y).map(Math.floor);
         // call diff2 version
-        var res = diff2(x, y, eps);
+        var res = extremaXY(x, y, eps);
         res.minlist = res.minlist.map(function(val) {
-            return Math.floor((val[1] + val[0]) / 2);
+            var index = Math.floor((val[1] + val[0]) / 2);
+            return Object.keys(values)[index];
         });
         res.maxlist = res.maxlist.map(function(val) {
-            return Math.floor((val[1] + val[0]) / 2);
+            var index = Math.floor((val[1] + val[0]) / 2);
+            return Object.keys(values)[index];
         });
 
         return {minlist: res.minlist, maxlist: res.maxlist};
@@ -53,7 +131,7 @@ var diff1, diff2;
     /**
      * Alternative version that takes vectors [x] and [y] instead of
      * [values] as input and returns intervals that contain local minima
-     * and local maxima.
+     * and local maxima. [x] elements can be of any type.
      *
      * @author Balint Morvai <balint@morvai.de>
      * @license http://en.wikipedia.org/wiki/MIT_License MIT License
@@ -62,7 +140,7 @@ var diff1, diff2;
      * @param eps
      * @returns {minlist: Array, maxlist: Array}
      */
-    diff2 = function(x, y, eps) {
+    extremaXY = function(x, y, eps) {
         // declare local vars
         var n, s, m, M, maxlist, minlist, i, j;
         // define x & y enumerated arrays
